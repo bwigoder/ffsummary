be905e4ba675681ccb0e7fe7ec18b238a989c034
Benjamin Wigoder <bwigoder@gmail.com>
2014-06-02 22:30:25 +0100

    serve json file

--- hello.py
+++ hello.py
@@ -1,8 +1,15 @@
 import os
 from flask import Flask
+import json
+
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/campaigns.json')
+def campaigns_json():
+	f = json.loads(open('campaigns.json').read())
+	flask.jsonify(**f)
