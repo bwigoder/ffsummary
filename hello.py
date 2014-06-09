6890b3842fd9926481ebbe318f03f0c4c53a8d8e
Benjamin Wigoder <bwigoder@gmail.com>
2014-06-09 17:27:10 +0100

    increase count from 30 > 100

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask, jsonify, render_template, url_for
+import requests
 import json
+import urllib2
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 @app.route('/')
-def hello_world():
-    return render_template('hello.html')
+def hello():
+	return render_template('hello.html')
